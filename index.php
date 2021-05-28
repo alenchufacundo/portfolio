4d679c1c984896edf2bd1add621c0994a89ca206
alenchufacundo <alenfacurios@gmail.com>
2021-05-28 14:04:52 -0300

    nuevos cambios

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+
+$pg="inicio";
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -16,34 +23,7 @@
 </head>
 
 <body id="index">
-    <header>
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container">
-                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="navbar-collapse collapse" id="navbarCollapse" style="">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0 mt-2">
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="index.html">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="sobre-mi.html">Sobre mí</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="proyectos.html">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="contacto.html">Contacto</a>
-                        </li>
-                    </ul>
-                    <div class="d-inline">
-                        <a href="" class="btn btn-rojo">Descargar mi CV <i class="fas fa-download"></i></a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
+<?php include_once ("header.php"); ?>
     <main>
         <div class="container">
             <div class="row">
