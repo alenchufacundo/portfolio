49377d852d1d49a97dc2f48dfe3655565a3f9fe7
alenchufacundorios <alenfacurios@gmail.com>
2022-08-18 19:49:45 -0300

    menu hamburguer

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 // Menu hamburguesa
 
-const toggleMenuElement = document.querySelector('.hamburguer__figure');
-const mainMenuElement = document.querySelector('.nav__menu');
-const exit = document.querySelector('.hamburguer__img--cross');
+const hamburguer = document.querySelector('.nav__menu--mobile');
+const menu__nav = document.querySelector('.nav__menu');
+const nav__list = document.querySelectorAll('.nav__list');
+const lines = document.querySelectorAll('.nav__mobile--line');
 
-toggleMenuElement.addEventListener('click', () => {
-  exit.classList.add('hamburguer__img--cross--show');
-  mainMenuElement.classList.add('nav__menu--show');
+
+
+// Funcionalidad para agregar las clases de css
+hamburguer.addEventListener('click', () => {
+  menu__nav.classList.toggle('active');
+  hamburguer.classList.toggle('active');
+  lines.forEach(child => {child.classList.toggle('animated')})
 });
 
+
+// Funcionalidad para quitar las clases de css
+nav__list.forEach(child => {
+  child.addEventListener('click', () => {
+    menu__nav.classList.remove('active');
+    hamburguer.classList.remove('active');
+    lines.forEach(item => {item.classList.remove('animated')}); 
+  });
+});
