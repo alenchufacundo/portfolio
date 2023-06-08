52241919101fde369a8a99f4089b3ffd8db60e14
alenchufacundo <alenfacurios@gmail.com>
2023-06-08 18:06:12 -0300

    last commit

--- sobre-mi.php
+++ sobre-mi.php
@@ -4,7 +4,6 @@ $pg="sobre-mi";
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -28,9 +27,8 @@ $pg="sobre-mi";
             <div class="row">
                 <div class="col-sm-5 col-12 mt-4">
                     <h1 class="mt-sm-4 pb-sm-4">Sobre mí</h1>
-                    <p class="pb-sm-5">Apasionado por la nutricion y los sistemas. Soy licenciado en Nutricion y
-                        estudiante de programacion.</p>
-                    <a href="contacto.php" class="btn btn-rojo">Enviar mensaje</a>
+                    <p class="pb-sm-5">Hola, soy Facundo, me recibi de licenciado en Nutricion en 2021, pero la pasion por la tecnologia y los sistemas me llevo al camino del desarrollo</p>
+                    <a href="contacto.php" class="btn btn-rojo">Enviame un mensaje!</a>
 
                 </div>
                 <div class="col-sm-3 col-12 mt-4 offset-sm-2">
@@ -55,30 +53,9 @@ $pg="sobre-mi";
                     </div>
                     <div class="col-sm-2 col-6 text-center">
                         <div class="px-4 py-5 card mx-0">
-                            <h3>PHP</h3>
-                            <img class="img-fluid d-block mx-auto" width="60"
-                                src="https://img.stackshare.io/service/991/hwUcGZ41_400x400.jpg">
-                        </div>
-                    </div>
-                    <div class="col-sm-2 col-6 text-center">
-                        <div class="px-4 py-5 card mx-0">
                             <h3>HTML5</h3>
                             <img class="img-fluid d-block mx-auto" width="60"
                                 src="https://img.stackshare.io/service/2538/kEpgHiC9.png">
-                        </div>
-                    </div>
-                    <div class="col-sm-2 col-6 text-center">
-                        <div class="px-4 py-5 card mx-0">
-                            <h3>React.js</h3>
-                            <img class="img-fluid d-block mx-auto" width="60"
-                                src="https://img.stackshare.io/service/1020/OYIaJ1KK.png">
-                        </div>
-                    </div>
-                    <div class="col-sm-2 col-6 text-center">
-                        <div class="px-4 py-5 card mx-0">
-                            <h3>jQuery</h3>
-                            <img class="img-fluid d-block mx-auto" width="60"
-                                src="https://img.stackshare.io/service/1021/lxEKmMnB_400x400.jpg">
                         </div>
                     </div>
                     <div class="col-sm-2 col-6 text-center">
@@ -90,18 +67,18 @@ $pg="sobre-mi";
                     </div>
                 </div>
                 <div class="row mt-1">
-                    <div class="col-sm-2 col-6">
-                        <div class="px-4 py-5 card mx-0">
-                            <h3>Laravel</h3>
-                            <img class="img-fluid d-block mx-auto" width="90"
-                                src="https://depcsuite.com/wp-content/uploads/2019/08/220px-Laravel.svg_.png">
-                        </div>
-                    </div>
                     <div class="col-sm-2 col-6 text-center">
                         <div class="px-4 py-5 card mx-0">
                             <h3>MySQL</h3>
                             <img class="img-fluid d-block mx-auto" width="60"
                                 src="https://img.stackshare.io/service/1615/mariadb-logo-400x400.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0">
+                            <h3>Python</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png">
                         </div>
                     </div>
                     <div class="col-sm-2 col-6 text-center">
@@ -113,23 +90,23 @@ $pg="sobre-mi";
                     </div>
                     <div class="col-sm-2 col-6 text-center">
                         <div class="px-4 py-5 card mx-0">
+                            <h3>LibreOffice base</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/LibreOffice_6.1_Base_Icon.svg/1200px-LibreOffice_6.1_Base_Icon.svg.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0">
+                            <h3>MS Access</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Microsoft_Office_Access_%282019-present%29.svg/800px-Microsoft_Office_Access_%282019-present%29.svg.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0">
                             <h3>Git</h3>
                             <img class="img-fluid d-block mx-auto" width="60"
                                 src="https://img.stackshare.io/service/1046/git.png">
-                        </div>
-                    </div>
-                    <div class="col-sm-2 col-6 text-center">
-                        <div class="px-4 py-5 card mx-0">
-                            <h3>Apache</h3>
-                            <img class="img-fluid d-block mx-auto" width="110"
-                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Apache_Software_Foundation_Logo_%282016%29.svg/1200px-Apache_Software_Foundation_Logo_%282016%29.svg.png">
-                        </div>
-                    </div>
-                    <div class="col-sm-2 col-6 text-center">
-                        <div class="px-4 py-5 card mx-0">
-                            <h3>Mercadopago</h3>
-                            <img class="img-fluid d-block mx-auto" width="60"
-                                src="https://img.stackshare.io/service/12698/gUjuxmmd_400x400.jpg">
                         </div>
                     </div>
                 </div>
@@ -172,7 +149,7 @@ $pg="sobre-mi";
         <section id="experiencia" class="container">
             <div class="row">
                 <div class="col-12 pt-5 pb-4">
-                    <h2><i class="fas fa-briefcase"></i> Experiencia laboral</h2>
+                    <h2><i class="fas fa-briefcase"></i></h2>
                 </div>
             </div>
             <div class="row shadow bg-white rounded">
